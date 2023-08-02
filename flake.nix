a85cc610d444a4106178e56b2103153e9dd548b2
hybras <24651269+hybras@users.noreply.github.com>
2023-08-02 14:42:52 -0400

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A Nix Environment for my website's theme";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.hugo
+                pkgs.asciidoctor-with-extensions
+              ];
+            };
+          };
+        });
+}
